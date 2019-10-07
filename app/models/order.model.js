0c206523a67ddbe219be773193f284208e9f6641
unknown <connorpower104@gmail.com>
2019-10-07 01:35:44 -0400

    adjust schema

--- app/models/order.model.js
+++ app/models/order.model.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const OrderSchema = mongoose.Schema({
     customer_first_name: String,
     customer_last_name: String,
-    order_placed_date: Date,
-    deliver_by: Date,
     items: Array,
 }, {
     timestamps: true
